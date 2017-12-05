ecdbc8e4ca2d27da59265beff318e6e09f80008d
Dachi Natsvlishvili <dachinat@gmail.com>
2017-12-05 16:30:30 +0400

    Stores requests in memory to avoid server overload

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ const ipc = require('electron').ipcMain
 const path = require('path')
 const url = require('url')
 
+// DB
+
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
